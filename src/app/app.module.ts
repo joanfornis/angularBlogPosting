8b8534999dd01943248b746ec9731e8f127f49c7
joan.fornis <joan.fornis@external.mecalux.com>
2020-11-24 09:58:20 +0100

    Creado blog-detail y blog-cards con CLI 'ng generate'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BlogDetailComponent } from './blog-detail/blog-detail.component';
+import { BlogCardsComponent } from './blog-cards/blog-cards.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlogDetailComponent,
+    BlogCardsComponent
   ],
   imports: [
     BrowserModule
