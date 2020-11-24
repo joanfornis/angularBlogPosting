87c4e1600956cdb288f81f2658d8119b0019ee09
joan.fornis <joan.fornis@external.mecalux.com>
2020-11-24 14:58:02 +0100

    arreglillos json

--- src/app/blog-detail/blog-detail.component.ts
+++ src/app/blog-detail/blog-detail.component.ts
@@ -10,23 +10,23 @@ export class BlogDetailComponent implements OnInit {
   postUrl = window.location.pathname;
   posts = [
     {
-      url: '/post/post1',
+      url: '/post1',
       title: 'Post1',
       subtitle: 'Subtitle Post1',
       content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
       image: 'https://picsum.photos/300/150?random=1'
     },
     {
-      url: '/post/post2',
+      url: '/post2',
       title: 'Post2',
-      subtitle: 'Subtitle Post1',
+      subtitle: 'Subtitle Post2',
       content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
       image: 'https://picsum.photos/300/150?random=2'
     },
     {
-      url: '/post/post3',
+      url: '/post3',
       title: 'Post3',
-      subtitle: 'Subtitle Post1',
+      subtitle: 'Subtitle Post3',
       content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
       image: 'https://picsum.photos/300/150?random=3'  
     }
